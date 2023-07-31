e69c0777c3bad24e027e2d4dad090096a41fb334
Kubilay <119971669+KubilayZengin@users.noreply.github.com>
2023-07-31 15:24:26 +0300

    Update Acrome.py
    GUI design changed.

--- Acrome.py
+++ Acrome.py
@@ -3,15 +3,15 @@ This source code is designed for Ball and Beam system by Acrome Robotics
 This source code has written in Python 3.7.4
 Author: Kubilay ZENGİN
 """
-import time
 
 # Library installation commands given below
 import customtkinter  # pip install customtkinter
 import serial  # pip install serial
 import numpy as np  # pip install numpy
 import matplotlib.pyplot as plt  # pip install matplotlib
-import PID  # PID class
 import serial.tools.list_ports
+import time  # pip install python-time
+import PID  # PID class
 from PIL import Image  # pip install pillow
 
 # Detect all available COMs
@@ -53,62 +53,62 @@ class App(customtkinter.CTk):
 
         self.label_3 = customtkinter.CTkLabel(self.frame, text="Step", font=Font_tuple_3, text_color="white")
         self.label_3.pack(padx=10, pady=12)
-        self.label_3.place(x=75, y=186)
+        self.label_3.place(x=75, y=386)
 
         self.label_4 = customtkinter.CTkLabel(self.frame, text="Sin", font=Font_tuple_3, text_color="white")
         self.label_4.pack(padx=10, pady=12)
-        self.label_4.place(x=180, y=186)
+        self.label_4.place(x=180, y=386)
 
         self.label_5 = customtkinter.CTkLabel(self.frame, text="Ramp", font=Font_tuple_3, text_color="white")
         self.label_5.pack(padx=10, pady=12)
-        self.label_5.place(x=267, y=186)
+        self.label_5.place(x=267, y=386)
 
         self.label_6 = customtkinter.CTkLabel(self.frame, text="Frequency (Hz):", font=Font_tuple_2, text_color="white")
         self.label_6.pack(padx=10, pady=12)
-        self.label_6.place(x=50, y=340)
+        self.label_6.place(x=50, y=540)
 
         self.label_7 = customtkinter.CTkLabel(self.frame, text="Amplitude (mm):", font=Font_tuple_2, text_color="white")
         self.label_7.pack(padx=10, pady=12)
-        self.label_7.place(x=50, y=300)
+        self.label_7.place(x=50, y=500)
 
         self.label_8 = customtkinter.CTkLabel(self.frame, text="Position\nControl",
                                               font=Font_tuple_2, text_color="white")
         self.label_8.pack(padx=10, pady=12)
-        self.label_8.place(x=60, y=410)
+        self.label_8.place(x=60, y=186)
 
         self.label_9 = customtkinter.CTkLabel(self.frame, text="Velocity\nControl", font=Font_tuple_2,
                                               text_color="white")
         self.label_9.pack(padx=10, pady=12)
-        self.label_9.place(x=150, y=410)
+        self.label_9.place(x=150, y=186)
 
-        self.label_10 = customtkinter.CTkLabel(self.frame, text="Serial Ports", font=Font_tuple_3,
+        self.label_10 = customtkinter.CTkLabel(self.frame, text="Serial Ports", font=Font_tuple_2,
                                                text_color="white")
         self.label_10.pack(padx=10, pady=12)
-        self.label_10.place(x=245, y=510)
+        self.label_10.place(x=245, y=200)
 
         self.label_11 = customtkinter.CTkLabel(self.frame, text="Stop Time", font=Font_tuple_2,
                                                text_color="white")
         self.label_11.pack(padx=10, pady=12)
-        self.label_11.place(x=245, y=430)
+        self.label_11.place(x=245, y=290)
 
         # Buttons
         self.button_1 = customtkinter.CTkButton(self.frame, text="", width=75, height=75, fg_color="#C41E3A",
                                                 image=customtkinter.CTkImage(dark_image=Image.open("images/step.png"),
                                                                              size=(50, 50)), command=self.step)
         self.button_1.pack(padx=10, pady=12)
-        self.button_1.place(x=50, y=210)
+        self.button_1.place(x=50, y=410)
 
         self.button_2 = customtkinter.CTkButton(self.frame, text="", width=75, height=75, fg_color="#C41E3A",
                                                 image=customtkinter.CTkImage(dark_image=Image.open("images/sin.png"),
                                                                              size=(50, 50)), command=self.sin)
         self.button_2.pack(padx=10, pady=12)
-        self.button_2.place(x=150, y=210)
+        self.button_2.place(x=150, y=410)
 
         self.button_3 = customtkinter.CTkButton(self.frame, text="", width=75, height=75, fg_color="#C41E3A",
                                                 image=customtkinter.CTkImage(dark_image=Image.open("images/ramp.png"),
                                                                              size=(50, 50)), command=self.step)
         self.button_3.pack(padx=10, pady=12)
-        self.button_3.place(x=250, y=210)
+        self.button_3.place(x=250, y=410)
 
         self.start_button = customtkinter.CTkButton(self.frame, text="", width=1, height=1, fg_color="#2B2B2B",
                                                     bg_color="#2B2B2B", command=self.start, border_width=0,
@@ -127,45 +127,45 @@ class App(customtkinter.CTk):
         # Entries
         self.entry_1 = customtkinter.CTkEntry(self.frame, placeholder_text="")
         self.entry_1.pack(padx=10, pady=12)
-        self.entry_1.place(x=185, y=300)
+        self.entry_1.place(x=185, y=500)
 
         self.entry_2 = customtkinter.CTkEntry(self.frame, placeholder_text="")
         self.entry_2.pack(padx=10, pady=12)
-        self.entry_2.place(x=185, y=340)
+        self.entry_2.place(x=185, y=540)
 
         self.entry_3 = customtkinter.CTkEntry(self.frame, placeholder_text="Kp", width=50, font=Font_tuple_3)
         self.entry_3.pack(padx=10, pady=12)
-        self.entry_3.place(x=65, y=460)
+        self.entry_3.place(x=65, y=240)
 
         self.entry_4 = customtkinter.CTkEntry(self.frame, placeholder_text="Ki", width=50, font=Font_tuple_3)
         self.entry_4.pack(padx=10, pady=12)
-        self.entry_4.place(x=65, y=500)
+        self.entry_4.place(x=65, y=280)
 
         self.entry_5 = customtkinter.CTkEntry(self.frame, placeholder_text="Kd", width=50, font=Font_tuple_3)
         self.entry_5.pack(padx=10, pady=12)
-        self.entry_5.place(x=65, y=540)
+        self.entry_5.place(x=65, y=320)
 
         self.entry_6 = customtkinter.CTkEntry(self.frame, placeholder_text="Kp", width=50, font=Font_tuple_3)
         self.entry_6.pack(padx=10, pady=12)
-        self.entry_6.place(x=150, y=460)
+        self.entry_6.place(x=150, y=240)
 
         self.entry_7 = customtkinter.CTkEntry(self.frame, placeholder_text="Ki", width=50, font=Font_tuple_3)
         self.entry_7.pack(padx=10, pady=12)
-        self.entry_7.place(x=150, y=500)
+        self.entry_7.place(x=150, y=280)
 
         self.entry_8 = customtkinter.CTkEntry(self.frame, placeholder_text="Kd", width=50, font=Font_tuple_3)
         self.entry_8.pack(padx=10, pady=12)
-        self.entry_8.place(x=150, y=540)
+        self.entry_8.place(x=150, y=320)
 
         self.entry_9 = customtkinter.CTkEntry(self.frame, placeholder_text="", width=100, font=Font_tuple_3)
         self.entry_9.pack(padx=10, pady=12)
-        self.entry_9.place(x=230, y=460)
+        self.entry_9.place(x=230, y=320)
         # Option menu
         self.com_port_menu = customtkinter.CTkOptionMenu(self.frame, values=available_coms, command=self.set_com,
                                                          width=80, fg_color="black", text_color="white",
                                                          button_color="#C41E3A", corner_radius=10)
         self.com_port_menu.pack(padx=10, pady=12)
-        self.com_port_menu.place(x=240, y=540)
+        self.com_port_menu.place(x=240, y=235)
 
     # Set GUI color function.
     @staticmethod
@@ -174,17 +174,6 @@ class App(customtkinter.CTk):
         customtkinter.set_appearance_mode(x)
         # Themes: "blue" (standard), "green", "dark-blue"
         customtkinter.set_default_color_theme(y)
-
-    # GUI starting coordinates function
-    def set_gui_position(self, width=500, height=800):
-        # Get screen width and height
-        screen_width = self.winfo_screenwidth()
-        screen_height = self.winfo_screenheight()
-
-        # Calculate position x and y coordinates
-        x = (screen_width / 2) - (width / 2)
-        y = (screen_height / 2) - (height / 2)
-        self.geometry('%dx%d+%d+%d' % (width, height, x - 400, y - 50))
 
     # Plot starting coordinates function
     @staticmethod
@@ -197,6 +186,17 @@ class App(customtkinter.CTk):
             f.canvas.manager.window.SetPosition((x, y))
         else:
             f.canvas.manager.window.move(x, y)
+
+    # GUI starting coordinates function
+    def set_gui_position(self, width=500, height=800):
+        # Get screen width and height
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+
+        # Calculate position x and y coordinates
+        x = (screen_width / 2) - (width / 2)
+        y = (screen_height / 2) - (height / 2)
+        self.geometry('%dx%d+%d+%d' % (width, height, x - 400, y - 50))
 
     # Step input function
     def step(self):
@@ -284,7 +284,6 @@ class App(customtkinter.CTk):
         fig, ax = plt.subplots(1, 1)
         # Call set_plot_position function
         self.set_plot_position(fig, 850, 145)
-        sample_size = 0
         k = True
         try:
             time_selected = int(self.entry_9.get())
@@ -326,7 +325,6 @@ class App(customtkinter.CTk):
                     plt.xlabel("Time (s)")
                     plt.pause(0.001)
 
-                    sample_size = sample_size + 1
             except KeyboardInterrupt:
                 # If the user presses Ctrl+F2, stop the program
                 print("Program terminated.")
